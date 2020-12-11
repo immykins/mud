8365baa2a340dfe9425de7f15fdeed22886f3e55
Imogen Kinsman <jade@thezets.com>
2020-12-10 18:12:11 -0800

    refactor login logic into a class

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const net = require('net')
 const { EventEmitter } = require('events')
 const { connect } = require('http2')
 
+const BANNER = require('./assets/banner').banner
+
 const GLOBAL_PUB_SUB = new EventEmitter()
 
 const PORT = 4444
@@ -10,16 +12,12 @@ const PORT = 4444
 const server = net.createServer((socket) => {
   const connection = new Connection()
   connection.on('line', (line) => {
-    console.log('listener')
     socket.write(`${line}\n`)
   })
   connection.process()
   const stream = through2(async function (data, _encoding, next) {
     const line = data.toString().trim()
     await connection.process(line)
-    // lines.forEach((line) => {
-    //   this.push(line)
-    // })
     next()
   })
   socket.pipe(stream).pipe(socket)
@@ -33,120 +31,72 @@ class Connection extends EventEmitter {
   constructor() {
     super()
     this.state = {}
-    // this.state = { connectionStage: connectionStages.NEW }
-    this.loginMachine = new LoginMachine(this.validateUser)
+    this.loginMachine = new LoginMachine(this.validateUserExists, this.authenticateUser)
   }
 
-  // get stage() {
-  //   return this.state.connectionStage
-  // }
+  // move these into a separate service that we can inject into Connection
+  async validateUserExists() {
+    return true
+  }
 
-  // set stage(state) {
-  //   this.state.connectionStage = state
-  // }
-
-  // line(line) {
-  //   this.emit('line', line)
-  // }
-
-  async validateUser() {
-
+  async authenticateUser() {
+    return false
   }
 
   async process(line) {
     if (!this.state.userInfo) {
-      // returns { lines: [],  }
-      // const response = this.loginMachine.next()
-      const { response, userInfo } = this.loginMachine.next()
-      console.log(`response is ${response}`)
+      const { response, userInfo } = await this.loginMachine.next()
+      this.state.userInfo = userInfo
       this.emit('line', response)
     }
-    // switch (this.stage) {
-    //   case connectionStages.NEW:
-    //     this.stage = connectionStages.PROMPTED
-    //     this.line('would you like to log in or create a new user ? ')
-    //     break
-    //   case connectionStages.PROMPTED:
-    //     if (line == "login") {
-    //       this.stage = connectionStages.LOGIN_PROMPT
-    //       this.process()
-    //     } else if (line == "new") {
-    //       this.stage = connectionStages.NEW_USER
-    //       this.process()
-    //     } else {
-    //       this.line('valid commands are login or new')
-    //     }
-    //     break
-    //   case connectionStages.LOGIN_PROMPT:
-    //     this.stage = connectionStages.LOGIN_USER
-    //     this.state.userInfo = {}
-    //     this.line('username:')
-    //     break
-    //   case connectionStages.NEW_USER:
-    //     this.line('')
-    //     break
-    //   case connectionStages.LOGIN_USER:
-    //     this.state.userInfo.user = line
-    //     this.stage = connectionStages.LOGIN_PASSWORD
-    //     this.line('password:')
-    //     break
-    //   case connectionStages.LOGIN_PASSWORD:
-    //     // this.line()
-    //     this.state.userInfo.password = line
-    //     const userID = await this.validateUser(this.state.userInfo)
-    //     if (userID) {
-    //       delete this.state.userInfo
-    //       this.state.userID = userID
-    //       GLOBAL_PUB_SUB.on(`user_${userID}`, this.line.bind(this))
-    //     } else {
-    //       this.line('login failed')
-    //       this.stage = connectionStages.LOGIN_PROMPT
-    //       this.process()
-    //     }
-    //     break
-    // }
   }
 }
 
 const connectionStages = {
   NEW: 'new',
-  PROMPTED: 'prompted',
   NEW_USER: 'user',
-  LOGIN_PROMPT: 'login_prompted',
+  LOGIN_PROMPTED: 'login_prompted',
   LOGIN_USER: 'login_user',
   LOGIN_PASSWORD: 'login_password'
 }
 
-// maintains the current login state and FSM logic
+const login_prompt = `
+Enter your account name or "new" to create a new account
+`
+
+const password_prompt = `
+Enter password
+`
+
+// Manages the login flow for a new connection.
 class LoginMachine {
-  constructor(validateFn) {
+  constructor(validateUserFn, authUserFn) {
     this.stage = connectionStages.NEW
-    this.validateFn = validateFn
+    this.validateFn = validateUserFn
+    this.authUserFn = authUserFn
   }
 
-  next() {
-    console.log('next')
+  async next() {
     switch (this.stage) {
       case connectionStages.NEW:
-        console.log("new")
-        this.stage = connectionStages.PROMPTED
-        return { response: 'would you like to log in or create a new user ?' }
-      case connectionStages.PROMPTED:
-        console.log("prompted")
-        // { line: }
-        this.stage = connectionStages.LOGIN_PROMPT
-        return { response: 'idk' }
-      // case connectionStages.NEW_USER:
-      // this.stage = connectionStages.LOGIN_U
-      // break
-      case connectionStages.LOGIN_PROMPT:
-        this.stage = connectionStages.LOGIN_USER
-        return { response: 'enter username:' }
-      case connectionStages.LOGIN_USER:
+        this.stage = connectionStages.LOGIN_PROMPTED
+        // TODO: make a text line display API to push lines to
+        return { response: [BANNER, login_prompt].join('\n') }
+      case connectionStages.LOGIN_PROMPTED:
+        // if line is NEW, we advance to NEW_USER
+        // else we await this.validateFn(line)
+        // if name doesn't exist, we re-prompt for name
+        // if it does exist, we advance to LOGIN_PASSWORD
         this.stage = connectionStages.LOGIN_PASSWORD
-        return { response: 'enter password' }
+        return { response: password_prompt }
       case connectionStages.LOGIN_PASSWORD:
-        break
+        // await this.authUserFn
+        // if succeeds:
+        return { response: 'password accepted', userInfo: {} }
+      // if fails:
+      // this.stage = connectionStages.LOGIN_PROMPTED
+      // return { response: ['Password incorrect', password_prompt].join('\n')}
+      // 
     }
   }
 }
