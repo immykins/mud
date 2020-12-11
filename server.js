9c489606a0eaa38f89fc9e3346bbe6a04b9fbfbc
Samantha Farmer <samus@samanthas-mbp.lan>
2020-12-10 20:07:27 -0800

    Processor pattern
    - add processor pattern
    - add eslint

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
 const through2 = require('through2')
 const net = require('net')
 const { EventEmitter } = require('events')
-const { connect } = require('http2')
 
 const BANNER = require('./assets/banner').banner
 
-const GLOBAL_PUB_SUB = new EventEmitter()
+// const GLOBAL_PUB_SUB = new EventEmitter()
 
 const PORT = 4444
+
+const connectionStages = {
+  NEW: 'new',
+  NEW_USER: 'user',
+  LOGIN_PROMPTED: 'login_prompted',
+  LOGIN_USER: 'login_user',
+  LOGIN_PASSWORD: 'login_password'
+}
+
+const loginPrompt = `
+Enter your account name or "new" to create a new account
+`
+
+const passwordPrompt = `
+Enter password
+`
 
 const server = net.createServer((socket) => {
   const connection = new Connection()
   connection.on('line', (line) => {
     socket.write(`${line}\n`)
   })
-  connection.process()
+
   const stream = through2(async function (data, _encoding, next) {
     const line = data.toString().trim()
     await connection.process(line)
@@ -27,77 +42,93 @@ server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-class Connection extends EventEmitter {
-  constructor() {
+class BannerProcessor extends EventEmitter {
+  process (_state, _line) {
+    this.emit('line', BANNER)
+    this.emit('processor:change', new LoginProcessor())
+  }
+}
+
+class LoginProcessor extends EventEmitter {
+  constructor () {
     super()
-    this.state = {}
-    this.loginMachine = new LoginMachine(this.validateUserExists, this.authenticateUser)
+    this.stage = connectionStages.NEW
+    this.userInfo = {}
   }
 
-  // move these into a separate service that we can inject into Connection
-  async validateUserExists() {
+  async validateUserExists () {
     return true
   }
 
-  async authenticateUser() {
+  async authenticateUser () {
     return false
   }
 
-  async process(line) {
-    if (!this.state.userInfo) {
-      const { response, userInfo } = await this.loginMachine.next()
-      this.state.userInfo = userInfo
-      this.emit('line', response)
-    }
-  }
-}
-
-const connectionStages = {
-  NEW: 'new',
-  NEW_USER: 'user',
-  LOGIN_PROMPTED: 'login_prompted',
-  LOGIN_USER: 'login_user',
-  LOGIN_PASSWORD: 'login_password'
-}
-
-const login_prompt = `
-Enter your account name or "new" to create a new account
-`
-
-const password_prompt = `
-Enter password
-`
-
-// Manages the login flow for a new connection.
-class LoginMachine {
-  constructor(validateUserFn, authUserFn) {
-    this.stage = connectionStages.NEW
-    this.validateFn = validateUserFn
-    this.authUserFn = authUserFn
-  }
-
-  async next() {
+  process (_state, line) {
     switch (this.stage) {
       case connectionStages.NEW:
         this.stage = connectionStages.LOGIN_PROMPTED
-        // TODO: make a text line display API to push lines to
-        return { response: [BANNER, login_prompt].join('\n') }
+        this.emit('line', loginPrompt)
+        break
       case connectionStages.LOGIN_PROMPTED:
-        // if line is NEW, we advance to NEW_USER
-        // else we await this.validateFn(line)
-        // if name doesn't exist, we re-prompt for name
-        // if it does exist, we advance to LOGIN_PASSWORD
+        this.userInfo.username = line
         this.stage = connectionStages.LOGIN_PASSWORD
-        return { response: password_prompt }
+        this.emit('line', passwordPrompt)
+        break
       case connectionStages.LOGIN_PASSWORD:
-        // await this.authUserFn
-        // if succeeds:
-        return { response: 'password accepted', userInfo: {} }
-      // if fails:
-      // this.stage = connectionStages.LOGIN_PROMPTED
-      // return { response: ['Password incorrect', password_prompt].join('\n')}
-      // 
+        this.userInfo.password = line
+        this.emit('line', 'password accepted')
+        this.emit('state:change', { userInfo: this.userInfo })
+        this.emit('processor:change', new MessagesProcessor())
+        break
     }
+  }
+}
+
+class MessagesProcessor extends EventEmitter {
+  process (state, _line) {
+    this.emit('line', `Not implemented yet -> ${state.userInfo.username}`)
+  }
+}
+class Connection extends EventEmitter {
+  constructor () {
+    super()
+    this.state = {}
+    this.processor = new BannerProcessor()
+  }
+
+  set processor (val) {
+    if (this._processor) {
+      this._processor.removeAllListeners('processor:change')
+      this._processor.removeAllListeners('line')
+      this._processor.removeAllListeners('state:change')
+      this._processor.removeAllListeners('advance')
+    }
+
+    this._processor = val
+    this._processor.on('processor:change', (processor) => {
+      this.processor = processor
+    })
+
+    this._processor.on('line', (line) => {
+      this.emit('line', line)
+    })
+
+    this._processor.on('state:change', (changes) => {
+      this.state = { ...this.state, ...changes }
+    })
+
+    process.nextTick(() => {
+      this.process()
+    })
+  }
+
+  get processor () {
+    return this._processor
+  }
+
+  async process (line) {
+    this.processor.process(this.state, line)
   }
 }
 
